ea81cc28ca529bf28343711cf5c71d4ac55b2bcc
soaringwind <3163053781@qq.com>
2022-09-22 22:26:38 +0800

    add

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,4 +1,14 @@
 #include <stdio.h>
-main(void){
-   printf("Hello world");
+#include <iostream>
+
+using namespace std;
+int main(void)
+{
+
+   int arr[5] = {1,2,3,4,5};
+   cout << arr << endl;
+   cout << &arr[0] << endl;
+   printf("Hello world\n");
+   cout << "hello world" << endl;
+   return 0;
 }
