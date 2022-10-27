cb4c53ce80a2f61dc0fd6f51ecf16024b0a23480
soaringwind <3163053781@qq.com>
2022-10-27 23:47:11 +0800

    add

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <deque>
+#include <algorithm>
 
 using namespace std;
 int main(void)
 {
-   int arr[5] = {1,2,3,4,5};
-   cout << arr << endl;
-   cout << &arr[0] << endl;
-   printf("Hello world\n");
-   cout << "hello world" << endl;
+   vector<int> v1;
+   v1.emplace_back(1);
+   v1.emplace_back(1);
+   vector<int>::iterator it = v1.end();
+   cout << *it << endl;
+   deque<int> d1;
    return 0;
 }
