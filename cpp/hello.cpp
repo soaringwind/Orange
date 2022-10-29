39dd2975dda6d5865fc00548ac12a82e300ece4c
soaringwind <3163053781@qq.com>
2022-10-29 23:24:29 +0800

    add

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -4,15 +4,17 @@
 #include <vector>
 #include <deque>
 #include <algorithm>
+#include "boost/version.hpp"
 
 using namespace std;
 int main(void)
 {
-   vector<int> v1;
-   v1.emplace_back(1);
-   v1.emplace_back(1);
-   vector<int>::iterator it = v1.end();
-   cout << *it << endl;
-   deque<int> d1;
+   cout << BOOST_VERSION << endl;
+   // vector<int> v1;
+   // v1.emplace_back(1);
+   // v1.emplace_back(1);
+   // vector<int>::iterator it = v1.end();
+   // cout << *it << endl;
+   // deque<int> d1;
    return 0;
 }
