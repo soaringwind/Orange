d95c8c33b662b1e43e8f2d6840c61f5b394a46e6
soaringwind <3163053781@qq.com>
2022-10-31 23:39:01 +0800

    add

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -4,17 +4,44 @@
 #include <vector>
 #include <deque>
 #include <algorithm>
+#include <map>
 #include "boost/version.hpp"
 
 using namespace std;
+
+
+class hello
+{
+private:
+   /* data */
+public:
+   hello(/* args */);
+   ~hello();
+};
+
+hello::hello(/* args */)
+{
+}
+
+hello::~hello()
+{
+}
+
+
 int main(void)
 {
-   cout << BOOST_VERSION << endl;
+   // cout << BOOST_VERSION << endl;
    // vector<int> v1;
    // v1.emplace_back(1);
    // v1.emplace_back(1);
    // vector<int>::iterator it = v1.end();
    // cout << *it << endl;
    // deque<int> d1;
+   map<string, int> m1;
+   hello p1;
+   m1.insert(pair<string, int>("1", 1));
+   m1.insert(make_pair("2", 2));
+   m1["2"] = 3;
+   cout << m1["2"] << endl;
    return 0;
 }
