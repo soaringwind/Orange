9a67ca2f9566544ef7dd156f3d37912d6d6ee67e
soaringwind <3163053781@qq.com>
2022-11-07 23:36:40 +0800

    Update dataframe.cpp

--- cpp/dataframe/dataframe.cpp
+++ cpp/dataframe/dataframe.cpp
@@ -3,59 +3,16 @@
 #include <map>
 #include <iostream>
 
-
-class BaseColumn
-{};
-
-
-template<class T>
-class Column:public BaseColumn
-{
-public:
-    Column(std::string &colName, std::vector<T> &data)
-    {
-        this->m_column = colName;
-        this->m_vec = data;
-    }
-    std::string m_column;
-    std::vector<T> m_vec;
-};
+using namespace std;
 
 class MyDataFrame
 {
 public:
-    std::vector<std::string> m_columnVec;
-    std::vector<BaseColumn*> m_dataVec;
+    string colName;
     template<class T>
-    void insertColumn(std::string colName, std::vector<T> data)
-    {
-        Column<T>* col = new Column<T>(colName, data);
-        this->m_dataVec.emplace_back(col);
-        this->m_columnVec.emplace_back(colName);
-    }
-    template<class T>
-    std::vector<T> getColumn(std::string colName)
-    {
-        for (int i=0; i<this->m_columnVec.size(); i++)
-        {
-            if (this->m_columnVec[i] == colName) 
-            {
-                return this->m_dataVec[i];
-            }
-        }
-    }
+    static vector<T> data;
+private:
 };
 
-int main(){
-    MyDataFrame df;
-    std::string colName = "index";
-    std::vector<int> data;
-    for (int i=0; i< 10; i++) {
-        data.emplace_back(i);
-    }
-    df.insertColumn<int>(colName, data);
-    auto res = df.getColumn<int>(colName);
-    for (int i=0; i<res.size(); i++) {
-        std::cout << i << std::endl;
-    }
-}
+template<class T>
+T pi = T(3.14);
