cbdcaff8817acfcaeed4cd88be07bab08fde79aa
soaringwind <3163053781@qq.com>
2022-11-15 16:20:14 +0800

    Update dataframe.cpp

--- cpp/dataframe/dataframe.cpp
+++ cpp/dataframe/dataframe.cpp
@@ -1,4 +1,4 @@
-// #pragma once
+#pragma once
 #include <vector>
 #include <string>
 #include <map>
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <iostream>
 #include <type_traits>
+#include <fstream>
 
 enum ObjectId
 {
@@ -22,7 +23,7 @@ class Cell
 {
 public:
 	// 储存cell的值指针
-	void *value;
+	void* value;
 
 	// 储存cell的值类型
 	ObjectId dtype;
@@ -32,7 +33,7 @@ public:
 
 	// 修改cell值，传入的值可能有多种数据类型
 	template <class T>
-	void update_value(T &_in);
+	void update_value(T& _in);
 
 	// 修改cell数据类型
 	void update_dtype(int objId);
@@ -44,6 +45,7 @@ public:
 class Series
 {
 public:
+	// Series名
 	std::string name;
 };
 
@@ -63,22 +65,23 @@ public:
 	std::vector<int> dtypeList;
 
 	// 储存数据
-	std::vector<std::vector<Cell *>> data;
+	std::vector<std::vector<Cell*>> data;
 
 	// 构造函数
 	DataFrame();
 
 	// 插入列，列可能有多种数据结构
-	void insert(std::string &&columnName, std::vector<int> &data);
+	template<class T>
+	void insert(std::string&& columnName, std::vector<T>& data);
 
 	// 根据列名查找列
-	std::vector<std::string> find(std::string &&columnName);
+	std::vector<std::string> findCol(std::string&& columnName);
 
 	// 根据列名取出对应列
-	Series get(std::string &&columnName);
+	Series get(std::string&& columnName);
 
 	// 输出到csv文件
-	void to_csv(std::string &&keyName);
+	void to_csv(std::string&& fileName);
 
 	// 深拷贝函数
 	DataFrame copy();
@@ -90,7 +93,7 @@ private:
 };
 
 template <class T>
-inline void Cell::update_value(T &_in)
+inline void Cell::update_value(T& _in)
 {
 	if (std::is_same<int, T>::value)
 	{
@@ -167,76 +170,144 @@ std::string Cell::to_string()
 	switch (this->dtype)
 	{
 	case intergId:
-		return std::to_string(*(int *)this->value);
+		return std::to_string(*(int*)this->value);
 	case floatId:
-		return std::to_string(*(float *)this->value);
+		return std::to_string(*(float*)this->value);
 	case doubleId:
-		return std::to_string(*(double *)this->value);
+		return std::to_string(*(double*)this->value);
 	case longId:
-		return std::to_string(*(long *)this->value);
+		return std::to_string(*(long*)this->value);
 	case stringId:
-		return *(std::string *)this->value;
+		return *(std::string*)this->value;
 	default:
 		break;
 	}
 	return std::string();
 }
 Cell Cell::copy() {
-	Cell* newCell = new Cell{};
+	Cell newCell{};
 	switch (this->dtype)
 	{
 	case intergId:
 	{
 		int tem_i = *(int*)value;
-		newCell->value = &tem_i;
-		newCell->dtype = intergId;
+		newCell.value = &tem_i;
+		newCell.dtype = intergId;
 		break;
 	}
 	case floatId:
 	{
 		float tem_f = *(float*)value;
-		newCell->value = &tem_f;
-		newCell->dtype = floatId;
+		newCell.value = &tem_f;
+		newCell.dtype = floatId;
 		break;
 	}
 	case doubleId:
 	{
 		double tem_d = *(double*)value;
-		newCell->value = &tem_d;
-		newCell->dtype = doubleId;
+		newCell.value = &tem_d;
+		newCell.dtype = doubleId;
 		break;
 	}
 	case longId:
 	{
 		long tem_l = *(long*)value;
-		newCell->value = &tem_l;
-		newCell->dtype = longId;
+		newCell.value = &tem_l;
+		newCell.dtype = longId;
 		break;
 	}
 	case stringId:
 	{
-		std::string tem_s = *(std::string *)value;
-		newCell->value = &tem_s;
-		newCell->dtype = stringId;
+		std::string tem_s = *(std::string*)value;
+		newCell.value = &tem_s;
+		newCell.dtype = stringId;
 		break;
 	}
 	default:
 		break;
 	}
-	return *newCell;
+	return newCell;
 }
-
+inline DataFrame::DataFrame()
+{
+}
+template<class T>
+void DataFrame::insert(std::string&& columnName, std::vector<T>& data)
+{
+	auto newVec = new std::vector<Cell*>;
+	ObjectId Id = intergId;
+	if (std::is_same<int, T>::value)
+	{
+		Id = intergId;
+	}
+	else if (std::is_same<float, T>::value)
+	{
+		Id = floatId;
+	}
+	else if (std::is_same<double, T>::value)
+	{
+		Id = doubleId;
+	}
+	else if (std::is_same<long, T>::value)
+	{
+		Id = longId;
+	}
+	else if (std::is_same<std::string, T>::value)
+	{
+		Id = stringId;
+	}
+	else
+	{
+		std::cout << "无类型" << std::endl;
+		return;
+	}
+	for (int i = 0; i < data.size(); i++) {
+		// 如果不使用new，则开辟在栈区
+		Cell* newCell = new Cell{};
+		newCell->value = &data[i];
+		newCell->dtype = Id;
+		newVec->emplace_back(newCell);
+	}
+	this->columnList.emplace_back(columnName);
+	this->column_length += 1;
+	this->dtypeList.emplace_back(Id);
+	this->row_length = std::max(row_length, int(data.size()));
+	this->data.emplace_back(*newVec);
+}
+inline void DataFrame::to_csv(std::string&& fileName)
+{
+	// 暂且写在一起，且不做多线程优化
+	std::ofstream outFile;
+	std::stringstream ss;
+	outFile.open(fileName, std::ios::out | std::ios::trunc);
+	if (!outFile.good())
+	{
+		std::cout << "打开文件失败" << std::endl;
+		return;
+	}
+	for (int i = 0; i < this->data.size(); i++) {
+		for (int j = 0; j < this->data[i].size(); j++) {
+			ss << this->data[i][j]->to_string();
+			if (j != this->data[i].size() - 1) {
+				ss << ", ";
+			}
+		}
+		ss << "\n";
+	}
+	outFile << ss.str();
+	outFile.close();
+}
+inline DataFrame::~DataFrame()
+{
+}
 int main()
 {
-	Cell cell1{};
-	int a = 1;
-	cell1.value = &a;
-	cell1.dtype = intergId;
-	int b = 2;
-	cell1.update_value(b);
-	std::cout << cell1.to_string() << std::endl;
-	auto cell2 = cell1.copy();
-	cell2.update_value(a);
-	std::cout << cell2.to_string() << std::endl;
-	std::cout << cell1.to_string() << std::endl;
+	DataFrame df{};
+	std::vector<int> vec;
+	vec.emplace_back(1);
+	vec.emplace_back(2);
+	vec.emplace_back(1);
+	df.insert("first", vec);
+	std::cout << &df << std::endl;
+	df.to_csv("testdf.csv");
 }
